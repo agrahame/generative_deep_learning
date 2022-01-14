c1e549c8af58a2bfa7a1a4c26a0efef9599444e1
root <aalique.grahame@gmail.com>
2022-01-14 23:00:19 +0000

    Working on cyclegan training

--- chapter5/models/CycleGan.py
+++ chapter5/models/CycleGan.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from typing import List
+from fastai.vision.all import *
+from fastai.vision.gan import FixedGANSwitcher
 
 
 class Sampler(nn.Module):
@@ -67,7 +69,7 @@ class GeneratorUNet(nn.Module):
                  ds_kernel_size: int,
                  us_kernel_size: int,
                  ds_stride: int,
-                 us_stride,
+                 us_stride: int,
                  dropout_rate: int = 0):
         super().__init__()
         self.skip_inputs = {}
@@ -136,7 +138,48 @@ class GeneratorUNet(nn.Module):
     
     def forward(self, inputs):
         return self.model(inputs)
-            
+
+
+class CombndGens(nn.Module):
+    
+    def __init__(self,
+                 input_shape: List[int],
+                 filters: List[int],
+                 ds_kernel_size: int,
+                 us_kernel_size: int,
+                 ds_stride: int,
+                 us_stride: int,
+                 dropout_rate: int = 0
+                ):
+        super().__init__()
+        
+        self.gen_ab = GeneratorUNet(input_shape=input_shape,
+                                    filters=filters,
+                                    ds_kernel_size=ds_kernel_size,
+                                    us_kernel_size=us_kernel_size,
+                                    ds_stride=ds_stride,
+                                    us_stride=us_stride,
+                                    dropout_rate=dropout_rate)
+        self.gen_ba = GeneratorUNet(input_shape=input_shape,
+                                    filters=filters,
+                                    ds_kernel_size=ds_kernel_size,
+                                    us_kernel_size=us_kernel_size,
+                                    ds_stride=ds_stride,
+                                    us_stride=us_stride,
+                                    dropout_rate=dropout_rate)
+    
+    def forward(self, img_a, img_b):
+        fake_b = self.gen_ab(img_a)
+        fake_a = self.gen_ba(img_b)
+        
+        reconstr_a = self.gen_ba(fake_b)
+        reconstr_b = self.gen_ab(fake_a)
+        
+        img_a_id = self.gen_ba(img_a)
+        img_b_id = self.gen_ab(img_b)
+        
+        return fake_b, fake_a, reconstr_a, reconstr_b, img_a_id, img_b_id        
+
 
 class DiscriminatorUNet(nn.Module):
     
@@ -182,3 +225,99 @@ class DiscriminatorUNet(nn.Module):
     
     def forward(self, inputs):
         return self.model(inputs)
+
+
+class CombndDiscs(nn.Module):
+    
+    def __init__(self,
+                 input_shape: List[int],
+                 filters: List[int],
+                 kernel_size: int
+                ):
+        super().__init__()
+        
+        self.disc_a = DiscriminatorUNet(input_shape=input_shape,
+                                        filters=filters,
+                                        kernel_size=kernel_size)
+        self.disc_b = DiscriminatorUNet(input_shape=input_shape,
+                                        filters=filters,
+                                        kernel_size=kernel_size)
+    
+    def forward(self, img_a, img_b):
+        return self.disc_a(img_a), self.disc_b(img_b)
+
+
+class CycleGANModule(nn.Module):
+    
+    def __init__(self,
+                 generator: CombndGens = None,
+                 discriminator: CombndDiscs = None,
+                 gen_mode: bool = False
+                ):
+        super().__init__()
+        
+        if generator is not None: self.generator = generator
+        if discriminator is not None: self.discriminator = discriminator
+        
+    def switch(self, gen_mode=None):
+        self.gen_mode = not self.gen_mode if gen_mode is None else gen_mode
+    
+    def forward(self, *args):
+        if self.gen_mode:
+            return self.generator(*args)
+        
+        return self.discriminator(*args)
+
+
+class CycleGANLoss(CycleGANModule):
+    
+    def __init__(self,
+                 gan: CycleGANModule
+                 lambda_valid: int,
+                 lambda_reconstr: int,
+                 lambda_id: int
+                ):
+        super().__init__()
+        
+        self.gan = gan
+        self.lambda_valid = lambda_valid
+        self.lambda_reconstr = lambda_reconstr
+        self.lambda_id = lambda_id
+    
+    def generator(self, gen_outputs, img_a, img_b):
+        fake_b, fake_a, reconstr_a, reconstr_b, img_a_id, img_b_id = gen_outputs
+        batch_size = img_a[0]
+        img_size = img_a[-1]  # assuming square images
+        # this reflects the output shape of disc output after the 3 stride-2 convs
+        patch = img_size / 2**3
+        valid_target = torch.ones(batch_size, 1, patch, patch)
+        
+        disc_a_pred, disc_b_pred = self.gan.discriminator(fake_a, fake_b)
+        
+        valid_loss = nn.MSELoss()
+        reconstr_loss = nn.L1Loss()  # MAE
+        id_loss = nn.L1Loss()  # MAE
+        
+        loss = self.lambda_valid * (valid_loss(disc_a_pred, valid_target) + valid_loss(disc_b_pred, valid_target))
+        loss += self.lambda_reconstr * (reconstr_loss(reconstr_a, img_a) + reconstr_loss(reconstr_b, img_b))
+        loss += self.lambda_id * (id_loss(img_a_id, img_a) + id_loss(img_b_id, img_b))
+        
+        return loss
+    
+    def discriminator(self, disc_outputs, img_a, img_b):
+        disc_a_pred, disc_b_pred = disc_outputs
+        batch_size = img_a[0]
+        img_size = img_a[-1]  # assuming square images
+        # this reflects the output shape of disc output after the 3 stride-2 convs
+        patch = img_size / 2**3
+        valid_target = torch.ones(batch_size, 1, patch, patch)
+        fake_target = torch.zeros(batch_size, 1, patch, patch)
+        
+        fake_b, fake_a, _, _, _, _ = self.gan.generator(img_a, img_b)
+        
+        disc_loss = nn.MSELoss()
+        
+        disc_a_loss = 0.5 * (disc_loss(img_a, valid_target) + disc_loss(fake_a, fake_target))
+        disc_b_loss = 0.5 * (disc_loss(img_b, valid_target) + disc_loss(fake_b, fake_target))
+        
+        return 0.5 * (disc_a_loss + disc_b_loss)
